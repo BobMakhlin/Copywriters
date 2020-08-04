edcf021a72b5446713ed8ef7357d125b28e1ca81
Bob Makhlin <borismakhlin5@gmail.com>
2020-08-04 17:33:55 +0300

    Make function showUserInfo and showUserPosts async

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const copywritersBlock = document.querySelector('.copywriters');
 
-loadJson(usersUrl)
-    .then(copywriters => {
-        for (let copywriter of copywriters) {
-            let user = new User(copywriter.id, copywriter.name);
-            let copywriterDiv = user.toHtml();
-            copywritersBlock.append(copywriterDiv);
-        }
-    });
+loadUsers();
+
+
+async function loadUsers() {
+    let copywriters = await loadJson(usersUrl);
+    
+    for (let copywriter of copywriters) {
+        let user = new User(copywriter.id, copywriter.name);
+        let copywriterDiv = user.toHtml();
+        copywritersBlock.append(copywriterDiv);
+    }
+}
